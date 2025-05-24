53ab91a31beb52cd74a25ab7f44fafad30b09b0e
icemberg <abhiraams2004@gmail.com>
2025-05-24 11:09:01 +0530

    four

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -13,9 +13,7 @@ public class App
 { 
     public static void main( String[] args ) 
     { 
-        ChromeOptions options = new ChromeOptions();
-	options.addArguments("--user-data-dir=/tmp/chrome-user-data-" + System.currentTimeMillis());
-	WebDriver driver = new ChromeDriver(options);
+	WebDriver driver = new ChromeDriver();
         driver.get("https://www.saucedemo.com/"); 
         driver.manage().window().maximize(); 
         driver.findElement(By.id("user-name")).sendKeys("standard_user"); 
